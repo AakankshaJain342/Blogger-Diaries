bbcc977570183c2c12348050606594e2c693a864
adityachhaparia <adityachhaparia101@gmail.com>
2021-09-07 18:25:06 +0530

    Added base models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=30)
+  date = models.DateField()
+  followers = models.ManyToManyField(User, related_name="followers")
+
+class Post(models.Model):
+  body = models.TextField()
+  title = models.CharField(max_length=30)
+  blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="posts")
+  date = models.DateField()
+
+class Comment(models.Model):
+  text = models.TextField(max_length=200)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+  date = models.DateField(null=True)
+
