e76d4cb80fd4b4b70d4c1d05cac28c7bdf4107d5
adityachhaparia <adityachhaparia101@gmail.com>
2021-11-01 20:22:45 +0530

    Add blog pages

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 app_name="blogs"
 
-urlpatterns = []
+urlpatterns = [
+  path('', index, name='index'),
+  path('<int:blog_id>/', blog_view, name='blog_view'),
+  path('new/', blog_create, name='blog_create'),
+]
