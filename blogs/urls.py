364cea49f114697ce9a29e36646e863208282e6e
AakankshaJain342 <aakankshajain030402@gmail.com>
2021-11-03 17:14:12 +0530

    blog edit page

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   path('', index, name='index'),
   path('<int:blog_id>/', blog_view, name='blog_view'),
   path('new/', blog_create, name='blog_create'),
+  path('<int:blog_id>/edit/',blog_edit,name='edit_post'),
 ]
